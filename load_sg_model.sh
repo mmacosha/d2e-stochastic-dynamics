4b6c83584118a29e718e4b6b172ff015e4a3f5ea
user name <s2754864-eidf107@eidf107-host1.vms.os.eidf.epcc.ed.ac.uk>
2025-07-01 18:34:02 +0000

    feat: updatset setup files

--- load_sg_model.sh
+++ load_sg_model.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-MODEL=$1
+VERSION=$1
 NAME=$2
 OUTDIR=$3
-
 
 if [ ! -d $OUTDIR ]; then
   mkdir -p $OUTDIR
 fi
 
-
-
-wget -P $OUTDIR "https://api.ngc.nvidia.com/v2/models/nvidia/research/${MODEL}/versions/1/files/${NAME}"
-
+wget -P $OUTDIR "https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan${VERSION}/versions/1/files/${NAME}"
